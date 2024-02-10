a28fd98b2d23f228be6b6d32d25d3ce85d23f050
plantsmith <janedoe@example.com>
2024-02-09 16:38:18 -0800

    data

---
+++ shinysetup.R
@@ -0,0 +1,21 @@
+### Attach necessary packages
+library(shiny)
+library(tidyverse)
+library(sf)
+library(here)
+
+### Create the user interface:
+ui <- fluidPage()
+
+### Create the server function:
+server <- function(input, output) {}
+
+### Combine them into an app:
+shinyApp(ui = ui, server = server)
+
+
+redlining_sf <- read_sf(here('data/mappinginequality.gpkg')) %>%
+  janitor::clean_names() %>%
+  filter(city == "Los Angeles")
+
+ggplot()+geom_sf(data=redlining_sf)
